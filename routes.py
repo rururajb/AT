4b45d67c085a03080f7f8c2071b44760e579e3af
Evan <115374841+evanwporter@users.noreply.github.com>
2022-10-26 12:10:58 -0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
 from enums import exchanges
 
+# Unimplemented
+
 routes = [
     (exchanges.BINANCE, "BTCUSDT", "1h", "BollingerBandsStrategy"),
     (exchanges.BINANCE, "ETHUSDT", "1h", "BollingerBandsStrategy"),
